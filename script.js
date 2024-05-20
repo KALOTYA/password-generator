c653c20a8c0963548be8b12e545bba1264a3e57e
Ajay Kaloty <kalotya99@gmail.com>
2024-05-19 18:54:19 -0700

    Trying to debug other params

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function generatePassword() {
   var length = parseInt(prompt("how long would you like your password? (MUST CONTAIN BETWEEN 8 - 128 CHARECTERS:"));
   console.log("password length:" + length);
   
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Password length must be a number between 8 and 128.");
+    return "";
+  }
+
   //prompting user for password criteria
   var incLowercase = confirm("Do you want to include Lowercase?");
   var incUppercase = confirm("Do you want to include Uppercase?");
@@ -37,10 +42,23 @@ function generatePassword() {
   var specialChars = "!@#$%^&*()_+{}[]|;:,.<>?";
 
   var allChars = "";
-  if (incLowercase) allChars += lowercaseChars;
-  if (incLowercase) allChars += lowercaseChars;
-  if (incLowercase) allChars += lowercaseChars;
-  if (incLowercase) allChars += lowercaseChars;
+  var requiredChars = [];
+  if (incLowercase) {
+    allChars += lowercaseChars;
+    requiredChars.push(lowercaseChars[Math.floor(Math.random() * lowercaseChars.length)]);
+  }
+  if (incUppercase) {
+    allChars += uppercaseChars;
+    requiredChars.push(uppercaseChars[Math.floor(Math.random() * uppercaseChars.length)]);
+  }
+  if (incNumbers) {
+    allChars += numbersChars;
+    requiredChars.push(numbersChars[Math.floor(Math.random() * numbersChars.length)]);
+  }
+  if (incSpecial) {
+    allChars += specialChars;
+    requiredChars.push(specialChars[Math.floor(Math.random() * specialChars.length)]);
+  }
 
 
   //Generating the password
